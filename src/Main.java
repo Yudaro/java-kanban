d6443334ae177fdc8dd295349c893f83ecd54303
Грунин Александр <yudaroryuu@gmail.com>
2025-04-24 20:18:40 +0200

    Убраны лишнии import из метода main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import entities.Epic;
-import entities.Subtask;
-import manager.FileBackedTaskManager;
-import manager.InMemoryTaskManager;
-
 public class Main {
     public static void main(String[] args) {
     }
